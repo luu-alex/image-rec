891b22f0ec4902024eae25bf353dcaaaab35da56
luu-alex <luu.alex98@gmail.com>
2018-09-21 01:13:59 -0400

    adding stuff to index

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 //requiring routes
-var websiteRoutes = require('./routes/website')
 
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.use('/', websiteRoutes);
+app.get("/", function(req,res){
+  res.render("landing")
+})
 
 app.listen(process.env.PORT||"3030", process.env.IP||"127.0.0.1", function(){
     console.log('server started!');
