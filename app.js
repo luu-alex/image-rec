2c7009b681b9edaa88c6f73afebbbdf09034a89b
Alex <alex.luu@mail.utoronto.ca>
2018-09-22 01:46:51 -0400

    creating app

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var app = express();
 
 
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 app.get("/", function(req,res){
   res.render("landing")
 })
 
-app.listen(process.env.PORT||"3030", process.env.IP||"192.168.1.82", function(){
+app.listen(process.env.PORT||"3030", process.env.IP||"127.0.0.1", function(){
     console.log('server started!');
 })
