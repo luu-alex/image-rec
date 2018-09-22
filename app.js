4a293472d5800694279322e2af8ee5e1ffe40c73
Alex <alex.luu@mail.utoronto.ca>
2018-09-22 15:02:58 -0400

    yes

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var multer = require('multer');
 //requiring routes
 
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.text())
 
 app.get("/", function(req,res){
   res.render("landing");
 })
 app.post('/api/Upload', function(req, res){
-  res.send(req.body.chat+" "+req.body.chat);
+  console.log(req);
+  console.log(typeof req.body.text);
+  res.send(req.body.text);
 
 });
 
