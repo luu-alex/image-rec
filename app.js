44b92dad977daa8c7c4c24b01bc2f2e7ddee80a8
Alex <alex.luu@mail.utoronto.ca>
2018-09-22 16:44:58 -0400

    y

--- app.js
+++ app.js
@@ -25,7 +25,22 @@ app.use("/watson", watsonRoutes)
 app.get("/", function(req,res){
   res.render("landing");
 })
+app.post('/clarifai/img-input', function(req, res){
+  clarifai.models.predict(Clarifai.GENERAL_MODEL, req.body.link).then(
+    function(response){
+      res.send("success");
+    },
+    function(err){
+      res.send("error");
+    }
+  )
 
+})
+
+app.post('/api/Upload', upload.single('avatar'), function(req, res){
+  console.log(req.file);
+  console.log(req.body.text);
+})
 app.get("/home", function(req,res){
   res.render("home");
 })
