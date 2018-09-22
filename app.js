647a0ab5ff538f215243b798e450544fe40c9549
Alex <alex.luu@mail.utoronto.ca>
2018-09-22 14:29:23 -0400

    added body parser

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 //requiring routes
 
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.get("/", function(req,res){
-  res.render("landing")
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(process.env.PORT||"3030", process.env.IP||"127.0.0.1", function(){
+app.get("/", function(req,res){
+  res.render("landing");
+})
+app.post('/api/Upload', function(req, res){
+  res.send(req.body.chat+" "+req.body.chat);
+
+});
+
+
+app.listen(process.env.PORT||"3030", process.env.IP||"192.168.0.24", function(){
     console.log('server started!');
 })
